e69362d426b1537f1cb17c57fe92b847a3189727
hemant <hemantsingh5151@gmail.com>
2022-07-26 00:50:48 +0530

    a

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -8,13 +8,14 @@ const app = express();
 dotenv.config({ path: "./config.env" });
 app.use(cors());
 app.use(express.json());
-const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/nextflix-ui/build")));
+  app.use(express.static(path.join(__dirname, "/../netflix-ui/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "nextflix-ui", "build", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "..", "netflix-ui", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => {
@@ -37,5 +38,5 @@ mongoose
 app.use("/api/user", userRoutes);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("server started on port 5000");
+  console.log("server started at port ", port);
 });
